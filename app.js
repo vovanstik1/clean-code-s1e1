d19cbaafac487bce1eed5fc350f43ac428cfd7cf
vovanstik1 <160480356+vovanstik1@users.noreply.github.com>
2025-10-16 14:18:54 +0300

    refactor: 3.4. Tag selectors

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
   var listItem=document.createElement("li");
+  listItem.className = "task-item";
 
     //input (checkbox)
   var checkBox=document.createElement("input");//checkbx
@@ -37,14 +38,17 @@ var createNewTaskElement=function(taskString){
 
     //Each elements, needs appending
   checkBox.type="checkbox";
+  checkBox.className = "task-check";
+
   editInput.type="text";
-  editInput.className="task";
+  editInput.className = "task-input";
 
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className="edit";
+  editButton.className="edit btn";
 
-  deleteButton.className="delete";
+  deleteButton.className="delete btn";
   deleteButtonImg.src='./remove.svg';
+  deleteButtonImg.className = "delete-img"
   deleteButton.appendChild(deleteButtonImg);
 
 
